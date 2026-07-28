6f8b277c0d76d71587a76e4a0b7d1400242d25b1
reemoplays3 <reemoplays3@gmail.com>
2026-07-28 00:20:55 -0500

    Add revokeall command

---
+++ commands/revokeall.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const dataPath = path.join(__dirname, "..", "data", "keys.json");
+
+module.exports = {
+  ownerOnly: true,
+
+  data: new SlashCommandBuilder()
+    .setName("revokeall")
+    .setDescription("Revokes every key"),
+
+  async execute(interaction) {
+    const database = JSON.parse(
+      fs.readFileSync(dataPath, "utf8")
+    );
+
+    let revokedCount = 0;
+
+    for (const key of database.keys) {
+      if (!key.revoked) {
+        key.revoked = true;
+        key.revokedAt = Date.now();
+        revokedCount++;
+      }
+    }
+
+    fs.writeFileSync(
+      dataPath,
+      JSON.stringify(database, null, 2)
+    );
+
+    const embed = new EmbedBuilder()
+      .setTitle("All Keys Revoked")
+      .setDescription(
+        revokedCount === 0
+          ? "Every key was already revoked."
+          : `Revoked **${revokedCount}** key(s).`
+      )
+      .setTimestamp();
+
+    await interaction.reply({
+      embeds: [embed],
+      ephemeral: true
+    });
+  }
+};
